39a2d3447326c057a738327d8e7cdf0f12128f26
dickersonj87 <105454249+dickersonj87@users.noreply.github.com>
2022-09-05 19:42:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -13,13 +13,27 @@ axios(url)
         const $ = cheerio.load(html)
         const articles = []
         
-        $('.result-heading', html).each(function() {
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
+        $('.result-info', html).each(function() {
+            const title = $(this).find('.result-heading').text()
+            // if (title contains table, chair, or dresser then return results)
+            const titleLower = title.toLowerCase()
+            const table = titleLower.includes('table')
+            const chair = titleLower.includes('chair')
+            const dresser = titleLower.includes('dresser')
+            const url = $(this).find('.result-heading').find('a').attr('href')
+            const location = $(this).find('.result-hood').text()
+            const price = $(this).find('.result-price').text()
+            const priceClean = price.replace('$', '')
+            const priceInt = parseFloat(priceClean)
+            if (priceInt <= 150 && table == true || chair == true || dresser == true){
+               articles.push({
                 title,
-                url
-            })
+                url,
+                location,
+                priceInt
+            }) 
+            }
+            
         })
         console.log(articles)
     }).catch(err => console.log(err))
